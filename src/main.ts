0e55ba1532e549d29983121a0843fbfc3160e3ce
Richard Martins <me@richaar.dev>
2023-09-04 12:34:02 -0300

    update github action

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import * as core from "@actions/core";
 import FormData from "form-data";
-import { ApiResponse, request } from "./request";
+import { SquareCloudAPI } from "@squarecloud/api"
 import { zipProject } from "./zip";
 
 async function run(): Promise<void> {
@@ -16,21 +16,16 @@ async function run(): Promise<void> {
     const formadata = new FormData();
     formadata.append("file", buffer, { filename: "application.zip" });
 
-    request("POST", `/commit/${id}?restart=${restart}`, {
-      headers: {
-        Authorization: token,
-        ...formadata.getHeaders(),
-      },
-      body: formadata,
-    }).then((res) => {
-      core.debug(JSON.stringify(res));
-      if (res.code != "SUCCESS") {
-        console.log(JSON.stringify(res))
-        core.setFailed(`Square Cloud returned code: ${res.code}`);
+    const api = new SquareCloudAPI(token);
+    api.applications.get(id).then(async (application) => {
+      core.debug("Application: ", JSON.stringify(application));
+      if (application == undefined) {
+        core.setFailed("Square Cloud returned undefined");
+        return;
       }
 
-      console.log(res.message);
-    }).catch((err) => core.setFailed(err));
+      await application.commit(buffer, "application.zip", restart)
+    }).catch((err) => core.setFailed(err))
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message);
   }
